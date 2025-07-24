0da547dc4bdc4929519f420dde710582e1d769e0
MichaelFetene123 <nsr.673.15@amu.edu.et>
2025-07-24 03:03:01 -0700

    Update axios baseURL to deployed server
    Changed the axios instance baseURL from the local Firebase functions endpoint to the deployed Amazon API on render.com for production use.

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-d91ea/us-central1/api", 
+  // local instance of firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-d91ea/us-central1/api",
+
+  // deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-tye7.onrender.com",
 });
 
 export { axiosInstance };
